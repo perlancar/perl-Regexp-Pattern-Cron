dbaf82fb1581db0f03e5f2113b4e4f8be41f6678
perlancar (on pc-home) <perlancar@gmail.com>
2019-11-03 09:07:55 +0700

    Initial commit

---
+++ lib/Regexp/Pattern/Cron.pm
@@ -0,0 +1,28 @@
+package Regexp::Pattern::Cron;
+
+# DATE
+# VERSION
+
+our %RE = (
+    minute => {
+    },
+    hour => {
+    },
+    day_of_month => {
+    },
+    month => {
+    },
+    day_of_week => {
+    },
+);
+
+1;
+# ABSTRACT: Regexp patterns related to cron & crontab
+
+=head1 SEE ALSO
+
+L<Config::Crontab>
+
+L<Parse::Crontab>
+
+L<Pegex::Crontab>
